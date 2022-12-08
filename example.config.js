76f4f2d827533c35a31c1f4dbf00123a1af4ae86
Disney <feibilanceon@gmail.com>
2022-12-08 21:26:59 +0800

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,7 +1,7 @@
 const config = {
   token: "12345:asdasda", // https://core.telegram.org/bots/faq#how-do-i-create-a-bot
   apiKey: "sk-asdasdasdasd", // https://beta.openai.com/account/api-keys
-  session: "asdasdasd", // https://chat.openai.com/chat 
+  session: "asdasdasd", // https://chat.openai.com/chat  (get your session_token from console)
 };
 
 export default config;
