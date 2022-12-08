72e0bcbd30028874201a8d05880fdda76f066214
disney <disney@tokeninsight.com>
2022-12-09 02:15:47 +0800

    remove config

--- example.config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  token: "12345:asdasda", // https://core.telegram.org/bots/faq#how-do-i-create-a-bot
-  apiKey: "sk-asdasdasdasd", // https://beta.openai.com/account/api-keys
-  session: "asdasdasd", // https://chat.openai.com/chat  (get your session_token from console)
-};
-
-export default config;
